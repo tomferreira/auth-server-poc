3a53c17f1b6db4d0e16414a5915c4ffb64f90a33
Cirillo Ferreira <cirillo.ferreira@gmail.com>
2022-05-30 15:35:12 -0300

    Update self-signed certs

--- keycloak/generate-certs.sh
+++ keycloak/generate-certs.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 set -eu
 
-cd certs
 org=localhost
 domain=localhost
 
-sudo trust anchor --remove "$domain".crt || true
+sudo trust anchor --remove ./certs/"$domain".crt || true
 
-openssl genpkey -algorithm RSA -out "$domain".key
+openssl genpkey -algorithm RSA -out ./certs/"$domain".key
 
-openssl req -x509 -key "$domain".key -out "$domain".crt \
+openssl req -x509 -days 365 -key ./certs/"$domain".key -out ./certs/"$domain".crt \
     -subj "/CN=$domain/O=$org" \
     -config <(cat /etc/ssl/openssl.cnf - <<END
 [ x509_ext ]
 basicConstraints = critical,CA:true
 subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid:always,issuer
-subjectAltName = DNS:$domain
+subjectAltName = @alternate_names
+[ alternate_names ]
+DNS.1      = $domain
+IP.1       = 127.0.0.1
+IP.2       = 10.0.2.2
 END
     ) -extensions x509_ext
 
-sudo trust anchor "$domain".crt
+sudo trust anchor ./certs/"$domain".crt || true
+
+cp ./certs/"$domain".crt ../mobile-app/android/app/src/main/res/raw/ -R
+
+echo "Para instalar o certificado .crt no app, siga o tutorial abaixo:"
+echo "https://medium.com/@noumaan/ssl-app-dev-a2923d5113c6"
